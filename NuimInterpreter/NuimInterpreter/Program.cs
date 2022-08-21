25dc0cd65605fecef1c7eef65b20afffa27a5fae
Luna Kilborn <leland.kilborn@hotmail.com>
2022-08-21 15:25:09 -0400

    Create Initial C# Project
    I used Visual Studio 2022 to create a basic Console app and a .gitignore

---
+++ NuimInterpreter/NuimInterpreter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NuimInterpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
