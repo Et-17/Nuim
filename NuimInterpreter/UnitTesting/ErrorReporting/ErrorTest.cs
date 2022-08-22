576aea2414abf93c4a7093d87ca9a3d7ab0e6cce
Luna Kilborn <leland.kilborn@hotmail.com>
2022-08-21 20:41:47 -0400

    Add testing for generic error type

---
+++ NuimInterpreter/UnitTesting/ErrorReporting/ErrorTest.cs
@@ -0,0 +1,20 @@
+﻿using NuimInterpreter.ErrorReporting;
+
+namespace UnitTesting.ErrorReporting
+{
+    [TestClass]
+    public class ErrorTest
+    {
+        [TestMethod]
+        public void InvalidLineNumber()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Error(-5));
+        }
+
+        [TestMethod]
+        public void Construction()
+        {
+            Assert.AreEqual(new Error(15).LineNumber, 15);
+        }
+    }
+}
